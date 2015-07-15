1c647e646361cb94a90feea353169f8442fa9700
Peter-Josef Meisch <pj.meisch@sothawo.com>
2015-07-15 16:35:43 +0200

    added CORS filter; load initial bookmarks

--- client/angularjs-bootstrap/app/app.js
+++ client/angularjs-bootstrap/app/app.js
@@ -6,21 +6,61 @@
 
 var app = angular.module('taboo', []);
 
+// create Controller that creates a ViewModel
 app.controller('BookmarksCtrl', function ($scope, $http) {
     $scope.vm = new BookmarksVM($http);
 });
 
+
+// ViewModel
 function BookmarksVM($http) {
     var that = this;
-    this.bookmarks = new Array();
+    this.bookmarks = [];
+    this.selectedTagList = [];
 
     that.bookmarks.push(
         new Bookmark('1', 'www.b1.de', 'b1', ['tag1-1', 'tag-1-2']),
         new Bookmark('2', 'www.b2.de', 'b2', ['tag2-1', 'tag-2-2'])
     );
+
+    /**
+     * clears all selection data
+     */
+    this.clearSelection = function() {
+        // clear the search field
+        that.setSelectedTags([]);
+    }
+
+    /**
+     * sets the selected tags and loads the bookmarks for the tags.
+     * @param tags the new selected tags
+     */
+    this.setSelectedTags = function(tags) {
+        if(Array.isArray(tags)) {
+            that.selectedTagList = tags;
+        } else {
+            that.selectedTagList = [];
+        }
+        that.reloadBookmarks();
+    }
+
+    /**
+     * reloads the bookmarks for the given selection criteria
+     */
+    this.reloadBookmarks = function() {
+        $http.get("http://localhost:8081/taboo/bookmarks")
+            .then(function(result){
+                that.bookmarks = result.data;
+            }).catch(function(result){
+                alert("Fehler: " + result.status + " " + result.statusText);
+            });
+    }
+
+    //initial setup
+    that.clearSelection();
 }
 
-
+// Bookmark class
 function Bookmark(id, url, title, tags) {
     this.id = id;
     this.url = url;
---
+++ service/spring-boot/src/main/java/com/sothawo/taboo/service/springboot/SimpleCORSFilter.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2015 sothawo
+ *
+ * http://www.sothawo.com
+ */
+package com.sothawo.taboo.service.springboot;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Filter that sets CORS headers in the response.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+            ServletException {
+        Collection<String> allowedOrigins = Arrays.asList("localhost", "127.0.0.1");
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        String origin = Optional.ofNullable(request.getHeader("Origin")).orElse("");
+
+        if (!origin.isEmpty()) {
+            try {
+                URL url = new URL(origin);
+                String host = url.getHost();
+                if (allowedOrigins.contains(host)) {
+                    HttpServletResponse response = (HttpServletResponse) res;
+                    response.setHeader("Access-Control-Allow-Origin", origin);
+                    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+                    response.setHeader("Access-Control-Max-Age", "3600");
+                    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+                    response.setHeader("Vary", "Origin");
+                }
+            } catch (MalformedURLException ignored) {
+                // ignored
+            }
+        }
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+
+}
